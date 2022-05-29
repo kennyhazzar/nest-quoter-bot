cfd4e264739bf5730f1a1eead80f95b65f77bb5a
kennyhazzar <56494597+kennyhazzar@users.noreply.github.com>
2022-05-29 15:14:47 +0700

    add axios, add mongoose module, add telegraf module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TelegrafModule } from 'nestjs-telegraf';
 import { GoogleService } from './google/google.service';
 
 @Module({
@@ -7,6 +9,10 @@ import { GoogleService } from './google/google.service';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ['env.development.local', 'env.production.local']
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    TelegrafModule.forRoot({
+      token: process.env.TELEGRAM_TOKEN,
     })
   ],
   controllers: [],

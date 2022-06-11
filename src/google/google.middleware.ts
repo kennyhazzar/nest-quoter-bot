6508d496c252de4b1289dd5b7bb1689144452c98
kennyhazzar <56494597+kennyhazzar@users.noreply.github.com>
2022-06-11 16:40:19 +0700

    google-routing: remove console logs

--- src/google/google.middleware.ts
+++ src/google/google.middleware.ts
@@ -7,16 +7,10 @@ export class TokenMiddleware implements NestMiddleware {
   constructor(private readonly googleService: GoogleService) {}
   async use(request: Request, response: Response, next: NextFunction) {
     const { expiresIn, updateTokenTimestamp } = this.googleService;
-    console.log(
-      `check token...\nlast update: ${updateTokenTimestamp}\nexpiresIn: ${expiresIn}\n'Date.now' - 'expiresIn' = '${
-        Date.now() - updateTokenTimestamp
-      }'`,
-    );
     if (Date.now() - updateTokenTimestamp >= expiresIn) {
       console.log('updated');
       await this.googleService.updateAccessToken();
     }
-    console.log('token is fresh now:)');
     next();
   }
 }

ec0c867e17b247c4ac4ef9a94eb56d4fab4c46a2
kennyhazzar <56494597+kennyhazzar@users.noreply.github.com>
2022-06-17 13:11:24 +0700

    feature/send-quote-logic: add send quote logic

--- src/crons/crons.service.ts
+++ src/crons/crons.service.ts
@@ -1,24 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import { SchedulerRegistry } from '@nestjs/schedule';
 import { InjectBot } from 'nestjs-telegraf';
+import { GoogleService } from 'src/google/google.service';
+import { getRandomInArray } from 'src/utils/random';
 import { Context, Telegraf } from 'telegraf';
 
 @Injectable()
 export class CronsService {
   constructor(
     private schedulerRegisty: SchedulerRegistry,
+    private readonly googleService: GoogleService,
     @InjectBot() private bot: Telegraf<Context>,
   ) {}
 
-  addInterval(name: string, milliseconds: number, userId: number) {
-    const testCallback = () => {
-      this.bot.telegram.sendMessage(
-        userId,
-        `Interval ${name} executing at time (${milliseconds})`,
+  async addInterval(name: string, milliseconds: number, userId: number) {
+    const sendQuoteCallback = async () => {
+      await this.googleService.actualizeSpreadsheet();
+      const lists = this.googleService.getSpreadsheetTitlesOfLists();
+
+      const { values: range } = await this.googleService.getCellByRange(
+        `'${lists[getRandomInArray(lists)]}'!A:ZZ`,
       );
+
+      const randomRange = range[getRandomInArray(range)].filter(
+        (item) => item !== '',
+      );
+      const quote = randomRange[getRandomInArray(randomRange)];
+
+      if (!quote) {
+        this.bot.telegram.sendMessage(
+          userId,
+          'Кажется, найденная сейчас рандомом цитата сломана, попробуем отправить лучше в следующий раз',
+          {
+            reply_markup: {
+              remove_keyboard: true,
+            },
+          },
+        );
+      }
+
+      this.bot.telegram.sendMessage(userId, quote, {
+        reply_markup: { remove_keyboard: true },
+      });
     };
 
-    const interval = setInterval(testCallback, milliseconds);
+    const interval = setInterval(sendQuoteCallback, milliseconds);
     this.schedulerRegisty.addInterval(name, interval);
   }
 
